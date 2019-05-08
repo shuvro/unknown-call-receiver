4a4d4f2c820b3bda9ee18f294f6821efcef08d15
Vikram Halder <vikramhalder@outlook.com>
2019-05-09 03:11:52 +0600

    Add Call Recorer

--- app/src/main/java/com/example/callautoreceiver/MainActivity.java
+++ app/src/main/java/com/example/callautoreceiver/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.callautoreceiver;
 
 import android.Manifest;
+import android.app.admin.DevicePolicyManager;
 import android.content.BroadcastReceiver;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Handler;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -15,9 +19,13 @@ import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -29,6 +37,14 @@ public class MainActivity extends AppCompatActivity {
     String phoneNumber;
     int state = -1;
     List<String[]> numberList = new ArrayList();
+    DevicePolicyManager mDPM;
+    ComponentName mAdminName;
+    int REQUEST_CODE = 0;
+    MediaRecorder recorder;
+    File audiofile;
+    boolean recordstarted = false;
+    static final String ACTION_IN = "android.intent.action.PHONE_STATE";
+    static final String ACTION_OUT = "android.intent.action.NEW_OUTGOING_CALL";
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
@@ -37,6 +53,15 @@ public class MainActivity extends AppCompatActivity {
         ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_PHONE_STATE}, 1);
         ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.PROCESS_OUTGOING_CALLS}, 1);
         ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_CONTACTS}, 1);
+        ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.INTERNET}, 1);
+        ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_NETWORK_STATE}, 1);
+        ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CHANGE_NETWORK_STATE}, 1);
+        ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.RECEIVE_BOOT_COMPLETED}, 1);
+        ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.WAKE_LOCK}, 1);
+        ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.RECEIVE_SMS}, 1);
+        ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_SMS}, 1);
+        ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+        ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.RECORD_AUDIO}, 1);
 
         textView = findViewById(R.id.textview);
         button = findViewById(R.id.button);
@@ -51,6 +76,7 @@ public class MainActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
+                record();
                 numberList = ServiceData.getContact(MainActivity.this);
             }
         }, 500);
@@ -83,13 +109,21 @@ public class MainActivity extends AppCompatActivity {
                 }, PhoneStateListener.LISTEN_CALL_STATE);
             }
         };
+
         registerReceiver(broadcastReceiver, new IntentFilter("android.intent.action.PHONE_STATE"));
+
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(ACTION_IN);
+        this.registerReceiver(new CallBr(), filter);
     }
 
     @Override
     public void onResume() {
         super.onResume();
         registerReceiver(broadcastReceiver, new IntentFilter("android.intent.action.PHONE_STATE"));
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(ACTION_IN);
+        this.registerReceiver(new CallBr(), filter);
     }
 
 
@@ -102,7 +136,7 @@ public class MainActivity extends AppCompatActivity {
                 Intent localIntent1 = new Intent(Intent.ACTION_HEADSET_PLUG);
                 localIntent1.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
                 localIntent1.putExtra("state", 1);
-                localIntent1.putExtra("microphone", 0);
+                localIntent1.putExtra("microphone", 1);
                 localIntent1.putExtra("name", "Headset");
                 sendOrderedBroadcast(localIntent1, "android.permission.CALL_PRIVILEGED");
 
@@ -119,32 +153,103 @@ public class MainActivity extends AppCompatActivity {
                 Intent localIntent4 = new Intent(Intent.ACTION_HEADSET_PLUG);
                 localIntent4.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
                 localIntent4.putExtra("state", 0);
-                localIntent4.putExtra("microphone", 0);
+                localIntent4.putExtra("microphone", 1);
                 localIntent4.putExtra("name", "Headset");
                 sendOrderedBroadcast(localIntent4, "android.permission.CALL_PRIVILEGED");
-                pickCall();
+
+
             } catch (Exception e2) {
                 e2.printStackTrace();
             }
         }
-        /*switch (state) {
-            case TelephonyManager.CALL_STATE_IDLE:
-                Toast.makeText(MainActivity.this, "Call Ended..", Toast.LENGTH_LONG).show();
-                break;
-            case TelephonyManager.CALL_STATE_OFFHOOK:
+    }
 
-                int outputBufferSize = AudioTrack.getMinBufferSize(160, AudioFormat.CHANNEL_IN_STEREO, AudioFormat.ENCODING_PCM_16BIT);
-                AudioTrack track = new AudioTrack(
-                        AudioManager.STREAM_VOICE_CALL,
-                        160,
-                        AudioFormat.CHANNEL_CONFIGURATION_MONO,
-                        AudioFormat.ENCODING_PCM_16BIT,
-                        outputBufferSize,
-                        AudioTrack.MODE_STREAM
-                );
-                break;
-            case TelephonyManager.CALL_STATE_RINGING:
-                break;
-        }*/
+    private void record() {
+        try {
+            mDPM = (DevicePolicyManager) getSystemService(Context.DEVICE_POLICY_SERVICE);
+            mAdminName = new ComponentName(this, DeviceAdminDemo.class);
+
+            if (!mDPM.isAdminActive(mAdminName)) {
+                Intent intent = new Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN);
+                intent.putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN, mAdminName);
+                intent.putExtra(DevicePolicyManager.EXTRA_ADD_EXPLANATION, "Click on Activate button to secure your application.");
+                startActivityForResult(intent, REQUEST_CODE);
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+
+    public class CallBr extends BroadcastReceiver {
+        Bundle bundle;
+        String state;
+        String inCall;
+        public boolean wasRinging = false;
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getAction().equals(ACTION_IN)) {
+                if ((bundle = intent.getExtras()) != null) {
+                    state = bundle.getString(TelephonyManager.EXTRA_STATE);
+                    if (state.equals(TelephonyManager.EXTRA_STATE_RINGING)) {
+                        inCall = bundle.getString(TelephonyManager.EXTRA_INCOMING_NUMBER);
+                        wasRinging = true;
+                        Toast.makeText(context, "IN : " + inCall, Toast.LENGTH_LONG).show();
+                    } else if (state.equals(TelephonyManager.EXTRA_STATE_OFFHOOK)) {
+                        if (wasRinging == true) {
+
+                            Toast.makeText(context, "ANSWERED", Toast.LENGTH_LONG).show();
+
+                            startRecord("incoming");
+                        }
+                    } else if (state.equals(TelephonyManager.EXTRA_STATE_IDLE)) {
+                        wasRinging = false;
+                        Toast.makeText(context, "REJECT || DISCO", Toast.LENGTH_LONG).show();
+                        if (recordstarted) {
+                            recorder.stop();
+                            recordstarted = false;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void startRecord(String seed) {
+            File sampleDir = new File(Environment.getExternalStorageDirectory(), "/unknown-call-recoded");
+            if (!sampleDir.exists()) {
+                sampleDir.mkdirs();
+            }
+            String file_name = "Record" + seed;
+            try {
+                audiofile = File.createTempFile(file_name, ".amr", sampleDir);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }  
+            recorder = new MediaRecorder();
+
+            recorder.setAudioSource(MediaRecorder.AudioSource.VOICE_COMMUNICATION);
+            recorder.setOutputFormat(MediaRecorder.OutputFormat.AMR_NB);
+            recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+            recorder.setOutputFile(audiofile.getAbsolutePath());
+            try {
+                recorder.prepare();
+            } catch (IllegalStateException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                recorder.start();
+                recordstarted = true;
+            } catch (Exception ex) {
+                recordstarted = false;
+            }
+        }
     }
 }
